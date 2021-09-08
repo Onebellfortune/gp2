8e3b9bc121e98e551d4b70b950c7e382d3648d92
Onebellfortune <37947858+Onebellfortune@users.noreply.github.com>
2021-09-08 18:50:06 +0900

    210908
    1.버튼 테스트

--- charity/src/js/app.js
+++ charity/src/js/app.js
@@ -5,22 +5,21 @@ var App = {
 
 $(window).load(function () {
 
-
-  if (typeof web3 !== 'undefined') {
+    if (typeof web3 !== 'undefined') {
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
-  } else { 
+    } else { 
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
-  }
+    }
 
-  $.getJSON("Charity.json", function (charity) {
+    $.getJSON("Charity.json", function (charity) {
       App.contracts.Charity = TruffleContract(charity);
       App.contracts.Charity.setProvider(App.web3Provider);
       render();
-  });
+    });
 
-  function render() {
+    function render() {
     //web3.currentProvider.selectedAddress
     web3.eth.getCoinbase(function (err, account) {
         if (err === null) {
@@ -40,18 +39,24 @@ $(window).load(function () {
                   var id = org[0];
                   var name = org[1];
                   var address=org[2];
-                  
+                  var perWei=10**18;
                   var res;
                   web3.eth.getBalance(address,function(e,r){
                         if(!e){
-                            var orgTable = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + address + "</td><td>"+r+"</td></tr>"
+                            var re=(r/perWei);
+                            re=re*100
+                            re=Math.floor(re)
+                            re=re/100
+                            re=re.toFixed(2)
+                            re=re.toLocaleString('ko-KR',2);
+                            //re.toPrecision(2);
+                            var orgTable = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + address + "</td><td>"+re+"</td><td>"+"ETH"+"</td></tr>"
                             $("#balances").append(orgTable);
                         }else{
 
                         }
                   })
                   
-                  console.log(res);
                   
 
                   var organizations="<option value='"+id+"'>"+name+"</option>"
@@ -64,9 +69,9 @@ $(window).load(function () {
       }).catch(function (error) {
           console.warn(error);
       });
-  }
+    }
 
-  $('#btnDonate').on('click',function(){
+    $('#btnDonate').on('click',function(){
       var uid=$('#organizationSelect').val()
       if(!uid){
           return alert('단체를 선택하세요')
@@ -97,4 +102,17 @@ $(window).load(function () {
             })
     })
 
+    $('#btnPopup').on('click',function(){
+        popup();
+    });
+    function popup(){
+        var url = "/popup.html";
+        var name = "기부 금액 설정";
+        var option = "width = 500, height = 500, top = 100, left = 200, location = no"
+        window.open(url, name, option);
+    }
+
+    $('#btnStart').on('click',function(){
+        location.href="/donation.html"
+    })
 });
